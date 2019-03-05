29d8c5fb82a3ae3d4691a24e4b46df379330aa72
skydoves <skydoves@naver.com>
2019-03-05 14:58:40 +0900

    modified HasActivityInjector implementation to DaggerApplication

--- app/src/main/java/com/wafflestudio/siksha/SikshaApplication.kt
+++ app/src/main/java/com/wafflestudio/siksha/SikshaApplication.kt
@@ -1,24 +1,25 @@
 package com.wafflestudio.siksha
 
-import android.app.Activity
-import android.app.Application
 import com.facebook.stetho.Stetho
 import com.wafflestudio.siksha.di.DaggerAppComponent
 import com.wafflestudio.siksha.di.EncoderModule
 import com.wafflestudio.siksha.di.PreferenceModule
 import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
+import dagger.android.DaggerApplication
 import timber.log.Timber
-import javax.inject.Inject
 
 @Suppress("SpellCheckingInspection")
-class SikshaApplication : Application(), HasActivityInjector {
+class SikshaApplication : DaggerApplication() {
 
-    @Inject
-    lateinit var activityInjector: DispatchingAndroidInjector<Activity>
+    private val appComponent = DaggerAppComponent.builder()
+        .application(this)
+        .preferenceModule(PreferenceModule(BuildConfig.PREF_KEY))
+        .encoderModule(EncoderModule(BuildConfig.JWT_SECRET))
+        .build()
 
-    override fun activityInjector(): AndroidInjector<Activity> = activityInjector
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return appComponent
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -27,12 +28,5 @@ class SikshaApplication : Application(), HasActivityInjector {
             Timber.plant(Timber.DebugTree())
             Stetho.initializeWithDefaults(this)
         }
-
-        DaggerAppComponent.builder()
-                .application(this)
-                .preferenceModule(PreferenceModule(BuildConfig.PREF_KEY))
-                .encoderModule(EncoderModule(BuildConfig.JWT_SECRET))
-                .build()
-                .inject(this)
     }
 }
