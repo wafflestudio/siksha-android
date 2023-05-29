95e108fcb70a3920c784edf147253563e49a7a02
sanggggg <jacob@vcnc.co.kr>
2023-05-29 14:25:31 +0900

    upgrade syntaxes

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+//    ext {
+//        kotlin_version = "1.4.31"
+//        navigation_version = "2.3.4"
+//        rxjava3_version = "3.0.0"
+//        retrofit_version = '2.9.0'
+//        arch_lifecycle_version = "2.3.0"
+//        room_version = "2.3.0-beta03"
+//        paging_version = "3.0.0-beta02"
+//        glide_version = "4.12.0"
+//    }
+plugins {
+    id("org.jlleitschuh.gradle.ktlint-idea") version "11.3.2" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2" apply false
+}
