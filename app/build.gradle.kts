35ca42de4a73c5d048ddd1b460f1cb48db0ab0e2
eastshine2741 <eastshine@snu.ac.kr>
2023-11-09 11:40:49 +0900

    shrinkResources, typo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("release")
         }
