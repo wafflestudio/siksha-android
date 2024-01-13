648658f3605b03ebaaed75e9e7119591c9bde728
송동엽 <eastshine@snu.ac.kr>
2024-01-13 22:41:19 +0900

    Release 3.1.0 (#53)
    * release Siksha 3.1.0-rc1
    
    * release Siksha 3.1.0
    
    * fix sementicVersionToSerializedCode
    
    * give change to sikshaVersion
    
    * release Siksha 3.1.0

--- buildSrc/src/main/kotlin/SemVer.kt
+++ buildSrc/src/main/kotlin/SemVer.kt
@@ -7,7 +7,7 @@ object SemVer {
         val minor = semVerRegex.find(semanticVersion)?.groupValues?.get(2)?.toLongOrNull() ?: 0L
         val patch = semVerRegex.find(semanticVersion)?.groupValues?.get(3)?.toLongOrNull() ?: 0L
         val releaseCandidate =
-            semVerRegex.find(semanticVersion)?.groupValues?.get(5)?.toLongOrNull() ?: 999L
+            semVerRegex.find(semanticVersion)?.groupValues?.get(5)?.toLongOrNull() ?: 99L
 
         return listOf(major, minor, patch, releaseCandidate)
             .fold(0) { acc, next ->
