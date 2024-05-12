5e32138c5565a42e85b55a1692d4e44cf91f3151
eastshine2741 <eastshine@snu.ac.kr>
2024-05-12 22:32:35 +0900

    좋아요 버튼 ripple effect 제거

--- app/src/main/java/com/wafflestudio/siksha2/components/compose/LikeIconWithCount.kt
+++ app/src/main/java/com/wafflestudio/siksha2/components/compose/LikeIconWithCount.kt
@@ -1,10 +1,12 @@
 package com.wafflestudio.siksha2.components.compose
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -27,7 +29,11 @@ fun LikeIconWithCount(
         modifier = modifier
             .then(
                 if (onClick != null) {
-                    Modifier.clickable(onClick = onClick)
+                    Modifier.clickable(
+                        onClick = onClick,
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() }
+                    )
                 } else {
                     Modifier
                 }
