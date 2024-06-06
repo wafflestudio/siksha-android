19a5427baa5f0551cdc3295ff35c1eae4a4de614
eastshine2741 <eastshine@snu.ac.kr>
2024-06-06 17:06:18 +0900

    ImageDetailFragment 상단에 현재 페이지 표시

--- app/src/main/java/com/wafflestudio/siksha2/ui/common/ImageDetailFragment.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/common/ImageDetailFragment.kt
@@ -5,7 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.wafflestudio.siksha2.R
 import com.wafflestudio.siksha2.databinding.FragmentImageDetailBinding
 
 class ImageDetailFragment : Fragment() {
@@ -26,6 +27,14 @@ class ImageDetailFragment : Fragment() {
 
     private val images by lazy { arguments?.getStringArrayList(ARG_IMAGES) }
 
+    private val onPageChangedCallback = object : OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            images?.let {
+                setPageText(position + 1, it.size)
+            }
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentImageDetailBinding.inflate(inflater)
         return binding.root
@@ -34,11 +43,18 @@ class ImageDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         images?.let {
             binding.vpImages.adapter = ImageDetailAdapter(it)
+            setPageText(binding.vpImages.currentItem + 1, it.size)
         }
+        binding.vpImages.registerOnPageChangeCallback(onPageChangedCallback)
+    }
+
+    private fun setPageText(currentPage: Int, pageCount: Int) {
+        binding.tvCurrentPage.text = context?.getString(R.string.image_detail_current_page, currentPage, pageCount)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        binding.vpImages.unregisterOnPageChangeCallback(onPageChangedCallback)
         _binding = null
     }
 }
