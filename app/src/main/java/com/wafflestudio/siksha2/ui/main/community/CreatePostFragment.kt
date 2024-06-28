b071d1290a5c8541849ec6dee81ded98464a31f6
wjshim2003 <wjshim@snu.ac.kr>
2024-06-28 09:59:29 +0900

    CreatePostFragment 기본 레이아웃

--- app/src/main/java/com/wafflestudio/siksha2/ui/main/community/CreatePostFragment.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/main/community/CreatePostFragment.kt
@@ -1,6 +1,27 @@
 package com.wafflestudio.siksha2.ui.main.community
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.wafflestudio.siksha2.R
+import com.wafflestudio.siksha2.ui.SikshaTheme
 
 class CreatePostFragment: Fragment() {
+    private val createPostViewModel: CreatePostViewModel by activityViewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_create_post, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<ComposeView>(R.id.community_compose_view).setContent {
+            SikshaTheme {
+                // TODO: CreatePostRoute 구현 후 여기에 연결
+            }
+        }
+    }
 }
