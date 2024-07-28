75c9c09174519c475fdf24b33b13cb816f224e47
wjshim2003 <wjshim@snu.ac.kr>
2024-07-28 15:34:27 +0900

    Create PostEditFragment

---
+++ app/src/main/java/com/wafflestudio/siksha2/ui/main/community/PostEditFragment.kt
@@ -0,0 +1,40 @@
+package com.wafflestudio.siksha2.ui.main.community
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.wafflestudio.siksha2.R
+import com.wafflestudio.siksha2.compose.ui.community.PostCreateRoute
+import com.wafflestudio.siksha2.ui.SikshaTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PostEditFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_post_create, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<ComposeView>(R.id.community_compose_view).setContent {
+            SikshaTheme {
+//                PostCreateRoute(
+//                    onNavigateUp = {
+//                        findNavController().navigateUp()
+//                    },
+//                    onUploadSuccess = { postId ->
+//                        findNavController().navigate(
+//                            PostCreateFragmentDirections.actionPostCreateFragmentToPostDetailFragment(
+//                                postId
+//                            )
+//                        )
+//                    }
+//                )
+            }
+        }
+    }
+}
