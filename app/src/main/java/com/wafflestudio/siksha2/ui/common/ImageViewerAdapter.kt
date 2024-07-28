c68c216ff3c62e8c2a1637ded4ff5b911cac9b7e
송동엽 <eastshine@snu.ac.kr>
2024-07-28 15:45:21 +0900

    이미지 뷰어 추가 (#71)
    * ScalableImageView 생성
    
    * scale 로직 수정, translation coerce 로직 추가
    
    * 원점 대신 포인터 중점을 기준으로 scale하도록 수정
    
    * Matrix 확장함수를 ScalableImageView 밖으로 이동
    
    * FIXME 주석 추가
    
    * ViewPager2로 ScalableImageView 보여주기
    
    * ImageDetailFragment 상단에 현재 페이지 표시
    
    * Fragment -> Activity 변경
    
    * ScalableImageView 로직 수정
    
    * ScalableImageView loading indicator 추가
    
    * ScalableImageView 주석 수정
    
    * ImageDetailFragment 삭제
    
    * ImageDetailActivity에서 ImageDetailAdapter 사용
    
    * 뷰페이저 중첩스크롤 기능 추가
    
    * 로그 삭제
    
    * initialPage extra 추가
    
    * status bar, navigation bar 투명 설정
    
    * PostDetailScreen에서 이미지 클릭 시 ImageViewer로 이동
    
    * ImageDetail -> ImageViewer rename
    
    * 닫기버튼 클릭리스너 추가
    
    * 불필요 코드 제거
    
    * lint

---
+++ app/src/main/java/com/wafflestudio/siksha2/ui/common/ImageViewerAdapter.kt
@@ -0,0 +1,35 @@
+package com.wafflestudio.siksha2.ui.common
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageViewerAdapter(private val images: List<String>) : RecyclerView.Adapter<ImageViewerViewHolder>() {
+    override fun getItemCount(): Int = images.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewerViewHolder {
+        val nestedScrollableHost = NestedScrollableHost(parent.context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            addView(
+                ScalableImageView(parent.context).apply {
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                }
+            )
+        }
+        return ImageViewerViewHolder(nestedScrollableHost)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewerViewHolder, position: Int) {
+        val scalableImageView = holder.nestedScrollableHost.getChildAt(0) as? ScalableImageView
+        scalableImageView?.setModel(images[position])
+    }
+}
+
+class ImageViewerViewHolder(
+    val nestedScrollableHost: NestedScrollableHost
+) : RecyclerView.ViewHolder(nestedScrollableHost)
