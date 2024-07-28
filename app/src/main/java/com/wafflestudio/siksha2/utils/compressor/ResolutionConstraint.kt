1c0e0b16187f4868a201503fd8ced3ef1c39b101
eastshine2741 <eastshine@snu.ac.kr>
2024-07-28 13:42:23 +0900

    zelory Compressor 복사

---
+++ app/src/main/java/com/wafflestudio/siksha2/utils/compressor/ResolutionConstraint.kt
@@ -0,0 +1,33 @@
+package com.wafflestudio.siksha2.utils.compressor
+
+import android.graphics.BitmapFactory
+import java.io.File
+
+/**
+ * Created on : January 24, 2020
+ * Author     : zetbaitsu
+ * Name       : Zetra
+ * GitHub     : https://github.com/zetbaitsu
+ */
+class ResolutionConstraint(private val width: Int, private val height: Int) : Constraint {
+
+    override fun isSatisfied(imageFile: File): Boolean {
+        return BitmapFactory.Options().run {
+            inJustDecodeBounds = true
+            BitmapFactory.decodeFile(imageFile.absolutePath, this)
+            calculateInSampleSize(this, width, height) <= 1
+        }
+    }
+
+    override fun satisfy(imageFile: File): File {
+        return decodeSampledBitmapFromFile(imageFile, width, height).run {
+            determineImageRotation(imageFile, this).run {
+                overWrite(imageFile, this)
+            }
+        }
+    }
+}
+
+fun Compression.resolution(width: Int, height: Int) {
+    constraint(ResolutionConstraint(width, height))
+}
