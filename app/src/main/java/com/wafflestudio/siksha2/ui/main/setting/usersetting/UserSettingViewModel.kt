25a900181a122da192b5c63dc91bb68968494e60
kevin990222 <113081855+kevin990222@users.noreply.github.com>
2024-08-18 17:44:27 +0900

    Comperssor util 반영

--- app/src/main/java/com/wafflestudio/siksha2/ui/main/setting/usersetting/UserSettingViewModel.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/main/setting/usersetting/UserSettingViewModel.kt
@@ -1,24 +1,18 @@
 package com.wafflestudio.siksha2.ui.main.setting.usersetting
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wafflestudio.siksha2.repositories.UserStatusManager
-import com.wafflestudio.siksha2.utils.PathUtil
+import com.wafflestudio.siksha2.utils.ImageUtil.getCompressedImage
 import dagger.hilt.android.lifecycle.HiltViewModel
-import id.zelory.compressor.Compressor
-import id.zelory.compressor.constraint.format
-import id.zelory.compressor.constraint.resolution
-import id.zelory.compressor.constraint.size
 import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
-import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
@@ -48,20 +42,13 @@ class UserSettingViewModel @Inject constructor(
     }
 
     suspend fun patchUserData(context: Context, nickname: String) {
-        val image = _imageUri.value?.let { getCompressedImage(context, it) }
+        val image = _imageUri.value?.let {
+            getCompressedImage(context, it)
+        }?.let { file ->
+            val requestBody = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            MultipartBody.Part.createFormData("image", file.name, requestBody)
+        }
         val updatedUserData = userStatusManager.updateUserProfile(nickname, image)
         _nickname.value = updatedUserData.nickname
-    }
-
-    private suspend fun getCompressedImage(context: Context, uri: Uri): MultipartBody.Part {
-        val path = PathUtil.getPath(context, uri)
-        var file = File(path)
-        file = Compressor.compress(context, file) {
-            resolution(300, 300)
-            size(100000)
-            format(Bitmap.CompressFormat.JPEG)
-        }
-        val requestBody = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
-        return MultipartBody.Part.createFormData("image", file.name, requestBody)
     }
 }
