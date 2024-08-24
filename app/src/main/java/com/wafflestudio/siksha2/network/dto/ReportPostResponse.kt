37dbbe8d78c6a9c3f7b353dcd437dcc35deffe20
junahjeon2002 <junahjeon2002@snu.ac.kr>
2024-08-24 16:21:41 +0900

    json 파싱 에러 수정 !

--- app/src/main/java/com/wafflestudio/siksha2/network/dto/ReportPostResponse.kt
+++ app/src/main/java/com/wafflestudio/siksha2/network/dto/ReportPostResponse.kt
@@ -1,10 +1,11 @@
 package com.wafflestudio.siksha2.network.dto
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class ReportPostResponse(
     val id: Long,
     val reason: String,
-    val postId: Long
+    @Json(name = "post_id") val postId: Long
 )
