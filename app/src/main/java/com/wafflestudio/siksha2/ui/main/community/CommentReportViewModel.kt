b79cfff712b185d7f841c7ca05813163117d1c53
eastshine2741 <eastshine@snu.ac.kr>
2024-08-31 03:02:01 +0900

    CommentReportViewModel에서 유저 정보 가져오기

--- app/src/main/java/com/wafflestudio/siksha2/ui/main/community/CommentReportViewModel.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/main/community/CommentReportViewModel.kt
@@ -3,9 +3,13 @@ package com.wafflestudio.siksha2.ui.main.community
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.wafflestudio.siksha2.models.User
 import com.wafflestudio.siksha2.repositories.CommunityRepository
+import com.wafflestudio.siksha2.repositories.UserStatusManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,13 +17,31 @@ import javax.inject.Inject
 @HiltViewModel
 class CommentReportViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val communityRepository: CommunityRepository
+    private val communityRepository: CommunityRepository,
+    private val userStatusManager: UserStatusManager
 ) : ViewModel() {
 
     private val _commentReportEvent = MutableSharedFlow<CommentReportEvent>()
     val commentReportEvent = _commentReportEvent.asSharedFlow()
 
     val commentId = CommentReportFragmentArgs.fromSavedStateHandle(savedStateHandle).commentId
+
+    private val _user = MutableStateFlow(User.Empty)
+    val user: StateFlow<User> = _user
+
+    init {
+        fetchUser()
+    }
+
+    private fun fetchUser() {
+        viewModelScope.launch {
+            runCatching {
+                _user.value = userStatusManager.getUserData()
+            }.onFailure {
+                // TODO: 예외 대응 필요
+            }
+        }
+    }
 
     fun reportComment(reportContent: String) {
         viewModelScope.launch {
