771a0592a94ca46c6147e98b479c15a425746b92
eastshine2741 <eastshine@snu.ac.kr>
2024-08-31 02:58:36 +0900

    PostReportViewModel에서 유저 정보 가져오기

--- app/src/main/java/com/wafflestudio/siksha2/models/User.kt
+++ app/src/main/java/com/wafflestudio/siksha2/models/User.kt
@@ -8,6 +8,10 @@ data class User(
     var id: Long,
     var nickname: String,
     var profileUrl: String?
-)
+) {
+    companion object {
+        val Empty = User(0L, "", null)
+    }
+}
 
 fun UserDto.toUser() = User(id = id, nickname = nickname, profileUrl = profileUrl)
--- app/src/main/java/com/wafflestudio/siksha2/ui/main/community/PostReportViewModel.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/main/community/PostReportViewModel.kt
@@ -3,9 +3,13 @@ package com.wafflestudio.siksha2.ui.main.community
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.wafflestudio.siksha2.models.User
 import com.wafflestudio.siksha2.repositories.CommunityRepository
+import com.wafflestudio.siksha2.repositories.UserStatusManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,13 +17,31 @@ import javax.inject.Inject
 @HiltViewModel
 class PostReportViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val communityRepository: CommunityRepository
+    private val communityRepository: CommunityRepository,
+    private val userStatusManager: UserStatusManager,
 ) : ViewModel() {
 
     private val _postReportEvent = MutableSharedFlow<PostReportEvent>()
     val postReportEvent = _postReportEvent.asSharedFlow()
 
     val postId = PostDetailFragmentArgs.fromSavedStateHandle(savedStateHandle).postId
+
+    private val _user = MutableStateFlow(User.Empty)
+    val user: StateFlow<User> = _user
+
+    init {
+        fetchUser()
+    }
+
+    private fun fetchUser() {
+        viewModelScope.launch {
+            runCatching {
+                _user.value = userStatusManager.getUserData()
+            }.onFailure {
+                // TODO: 예외 대응 필요
+            }
+        }
+    }
 
     fun reportPost(reportContent: String) {
         viewModelScope.launch {
