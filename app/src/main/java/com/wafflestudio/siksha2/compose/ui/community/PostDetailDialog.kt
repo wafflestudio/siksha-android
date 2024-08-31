3c21b980cda3bab0fee76b206f3b6abedd1ab79e
junahjeon2002 <junahjeon2002@snu.ac.kr>
2024-08-31 21:09:13 +0900

    lint

--- app/src/main/java/com/wafflestudio/siksha2/compose/ui/community/PostDetailDialog.kt
+++ app/src/main/java/com/wafflestudio/siksha2/compose/ui/community/PostDetailDialog.kt
@@ -13,12 +13,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import com.wafflestudio.siksha2.ui.SikshaColors
-import com.wafflestudio.siksha2.ui.SikshaTheme
 
 @Composable
 fun PostDetailDialog(
@@ -125,4 +123,3 @@ fun PostDetailDialog(
         }
     }
 }
-
