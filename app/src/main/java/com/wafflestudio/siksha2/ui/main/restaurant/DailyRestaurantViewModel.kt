1fc2fa22d29cfbc3ac3e5726e2af76c33930a38a
Shim Woojin <wjshim@snu.ac.kr>
2024-11-23 13:15:51 +0900

    Revert "축제 기능 추가 (#115)"
    This reverts commit 194d4de8dfd7a6f39992f3af760272075ed31832.

--- app/src/main/java/com/wafflestudio/siksha2/ui/main/restaurant/DailyRestaurantViewModel.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/main/restaurant/DailyRestaurantViewModel.kt
@@ -8,7 +8,6 @@ import androidx.lifecycle.viewModelScope
 import com.wafflestudio.siksha2.models.MealsOfDay
 import com.wafflestudio.siksha2.models.MenuGroup
 import com.wafflestudio.siksha2.models.RestaurantInfo
-import com.wafflestudio.siksha2.models.getRestaurantInfo
 import com.wafflestudio.siksha2.repositories.MenuRepository
 import com.wafflestudio.siksha2.repositories.RestaurantRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -44,9 +43,6 @@ class DailyRestaurantViewModel @Inject constructor(
 
     private val _favoriteRestaurantExists = MutableLiveData(false)
     val favoriteRestaurantExists: LiveData<Boolean> = _favoriteRestaurantExists
-
-    private val _showFestival = MutableLiveData(false)
-    val showFestival: LiveData<Boolean> = _showFestival
 
     private val showEmptyRestaurant = restaurantRepository.showEmptyRestaurant.asFlow()
     private val restaurantOrder = restaurantRepository.restaurantsOrder.asFlow()
@@ -111,10 +107,6 @@ class DailyRestaurantViewModel @Inject constructor(
         }
     }
 
-    fun toggleFestival() {
-        _showFestival.value = !showFestival.value!!
-    }
-
     fun getFilteredMenuGroups(showOnlyFavorite: Boolean): Flow<List<MenuGroup>> {
         return _dateFilter.asFlow()
             .flatMapLatest {
@@ -142,11 +134,6 @@ class DailyRestaurantViewModel @Inject constructor(
                 }
             }
             .map { it.filter { item -> item.isFavorite || showOnlyFavorite.not() } }
-            .map {
-                it.filter { item ->
-                    item.getRestaurantInfo().nameKr!!.startsWith("[축제]") == showFestival.value
-                }
-            }
             .combine(if (showOnlyFavorite) favoriteRestaurantOrder else restaurantOrder) { menuGroups, (order) ->
                 val result = mutableListOf<MenuGroup>()
                 val sortedMenuGroups = menuGroups.sortedByDescending { it.id }
