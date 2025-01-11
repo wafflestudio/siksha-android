299a3f434353dec23e10eb87408e25d244815793
kevin990222 <113081855+kevin990222@users.noreply.github.com>
2025-01-11 23:20:51 +0100

    Arrow_Down, check 버튼 추가

--- app/src/main/java/com/wafflestudio/siksha2/components/MenuFilterItem.kt
+++ app/src/main/java/com/wafflestudio/siksha2/components/MenuFilterItem.kt
@@ -3,6 +3,7 @@ package com.wafflestudio.siksha2.components
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
 import com.wafflestudio.siksha2.R
 import com.wafflestudio.siksha2.databinding.ItemMenuFilterBinding
@@ -30,10 +31,22 @@ class MenuFilterItem : LinearLayout {
         if (attributeSet != null) {
             val attributes = context.obtainStyledAttributes(attributeSet, R.styleable.MenuFilterItem)
             val text = attributes.getString(R.styleable.MenuFilterItem_filterText) ?: ""
+            val showArrow = attributes.getBoolean(R.styleable.MenuFilterItem_showArrow, false)
+            val showCheck = attributes.getBoolean(R.styleable.MenuFilterItem_showCheck, false)
             attributes.recycle()
 
+            showArrow(showArrow)
+            showCheck(showCheck)
             setText(text)
         }
+    }
+
+    fun showArrow(show: Boolean) {
+        binding.arrowDown.visibility = if (show) View.VISIBLE else View.GONE
+    }
+
+    fun showCheck(show: Boolean) {
+        binding.check.visibility = if (show) View.VISIBLE else View.GONE
     }
 
     fun setText(text: String) {
