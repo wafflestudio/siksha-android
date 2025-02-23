531a5d97f0241e2129014f99a8c4f98627a91ec9
junahjeon2002 <junahjeon2002@snu.ac.kr>
2025-02-23 10:43:25 +0900

    카테고리 추가, 디자인 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 
     defaultConfig {
         applicationId = "com.wafflestudio.siksha2"
-        minSdk = 30
+        minSdk = 31
         targetSdk = 34
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
