bb476a8d095943ad5da071ceea26c2ee0792e74e
Shim Woojin <wjshim@snu.ac.kr>
2025-04-03 20:44:37 +0900

    SplashActivity에 FeatureChecker 추가

--- app/src/main/java/com/wafflestudio/siksha2/ui/SplashActivity.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/SplashActivity.kt
@@ -18,6 +18,7 @@ import com.google.android.gms.common.api.Scope
 import com.google.android.gms.tasks.Task
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
+import com.wafflestudio.siksha2.FeatureChecker
 import com.wafflestudio.siksha2.R
 import com.wafflestudio.siksha2.databinding.ActivitySplashBinding
 import com.wafflestudio.siksha2.network.OAuthProvider
@@ -44,6 +45,9 @@ class SplashActivity : AppCompatActivity() {
 
     private lateinit var kakaoSignInLauncher: () -> Unit
 
+    @Inject
+    lateinit var featureChecker: FeatureChecker
+
     @InternalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,6 +62,8 @@ class SplashActivity : AppCompatActivity() {
                 finish()
                 return@launch
             }
+
+            featureChecker.fetchFeaturesConfig()
 
             if (checkLoginStatus().not()) {
                 binding.googleLoginButton.setVisibleOrGone(true)
