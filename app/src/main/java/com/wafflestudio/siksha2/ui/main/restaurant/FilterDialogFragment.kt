e06a7b0ac6c654920752e37540ffde90085069ef
오정윤 <165873171+nyunn2@users.noreply.github.com>
2025-04-14 20:21:46 +0900

    필터 디자인 수정 (#139)
    * 여백 수정, 말풍선 변경
    
    * 글꼴 수정
    
    * 여백 수정
    
    * Remove unnecessary files from PR
    
    * gradle 관련 파일 복구
    
    * Revert "gradle 관련 파일 복구"
    
    This reverts commit a5ad318e8f37990e189283c04a3a45b818f52c9f.
    
    * fix gitignore
    
    * Revert "Remove unnecessary files from PR"
    
    This reverts commit 6c285d45737c678fdca7d767c06836f90895d373.
    
    * fix gitignore
    
    * Delete .idea/deploymentTargetSelector.xml
    
    * Delete .idea/kotlinc.xml
    
    * Delete .idea/migrations.xml
    
    * 거리 슬라이더 말풍선 연결
    
    * Conlict 해결하다 놓친것들
    
    * 거리 랭지  text 해결
    
    * lint
    
    * 거리 말풍선 수정
    
    * 초기 위치 수정
    
    * lint
    
    ---------
    
    Co-authored-by: Shim Woojin <wjshim@snu.ac.kr>
    Co-authored-by: kevin990222 <113081855+kevin990222@users.noreply.github.com>

--- app/src/main/java/com/wafflestudio/siksha2/ui/main/restaurant/FilterDialogFragment.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/main/restaurant/FilterDialogFragment.kt
@@ -6,6 +6,8 @@ import android.content.res.Resources
 import android.graphics.Color
 import android.graphics.Outline
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +15,7 @@ import android.view.ViewGroup
 import android.view.ViewOutlineProvider
 import android.widget.GridLayout
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.os.bundleOf
 import androidx.core.view.children
 import androidx.fragment.app.activityViewModels
@@ -21,6 +24,7 @@ import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.chip.Chip
+import com.google.android.material.slider.RangeSlider
 import com.wafflestudio.siksha2.R
 import com.wafflestudio.siksha2.databinding.DialogFilterBinding
 import kotlinx.coroutines.launch
@@ -109,10 +113,22 @@ class FilterDialogFragment(
     }
 
     private fun setupDistanceSelection() {
+        val initialValue = listOf(selectedCondition.distance)
+        binding.distanceRangeSlider.setValues(initialValue)
+        updateDistanceText(selectedCondition.distance.toInt())
+        updateDistanceBubblePosition(binding.distanceRangeSlider)
+
+        binding.distanceRangeSlider.post {
+            updateDistanceBubblePosition(binding.distanceRangeSlider)
+        }
+
         binding.distanceRangeSlider.addOnChangeListener { slider, _, _ ->
             val value = slider.values[0]
             selectedCondition = selectedCondition.copy(distance = value)
             updateDistanceText(selectedCondition.distance.toInt())
+            Handler(Looper.getMainLooper()).post {
+                updateDistanceBubblePosition(slider)
+            }
         }
     }
 
@@ -159,6 +175,8 @@ class FilterDialogFragment(
     private fun setupCategorySelection() {
         binding.gridCategory.removeAllViews()
 
+        val nanumSquareBold = ResourcesCompat.getFont(requireContext(), R.font.nanum_square_bold)
+
         categorySet.forEachIndexed { index, category ->
             val chip = Chip(requireContext()).apply {
                 text = category
@@ -166,6 +184,8 @@ class FilterDialogFragment(
                 isClickable = true
                 isFocusable = true
                 checkedIcon = null
+
+                typeface = nanumSquareBold
 
                 layoutParams = GridLayout.LayoutParams().apply {
                     width = dpToPx(56) // 56dp
@@ -285,7 +305,23 @@ class FilterDialogFragment(
     }
 
     private fun updateDistanceText(distance: Int) {
-        binding.tvDistance.text = if (distance >= defaultCondition.distance.toInt()) "1km 이상" else "${distance}m 이내"
+        binding.tvDistanceRange.text = if (distance >= defaultCondition.distance.toInt()) {
+            "1km 이상"
+        } else {
+            "${distance}m 이내"
+        }
+    }
+
+    private fun updateDistanceBubblePosition(rangeSlider: RangeSlider) {
+        val valueRangeSize = rangeSlider.valueTo - rangeSlider.valueFrom
+        val valuePercent = (rangeSlider.values[0] - rangeSlider.valueFrom) / valueRangeSize
+        val valueXDistance = valuePercent * rangeSlider.trackWidth
+        val offset = rangeSlider.x + rangeSlider.trackSidePadding - (binding.distanceBubble.width / 2f)
+
+        val minX = rangeSlider.x + rangeSlider.trackSidePadding + 10f
+        val maxX = minX + rangeSlider.trackWidth - binding.distanceBubble.width + 10f
+
+        binding.distanceBubble.x = valueXDistance.coerceIn(minX, maxX) + offset
     }
 
     private fun updatePriceRangeText(minPrice: Int, maxPrice: Int) {
