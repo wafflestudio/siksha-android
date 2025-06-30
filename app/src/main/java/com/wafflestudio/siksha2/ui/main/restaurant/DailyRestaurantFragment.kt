c1805d0ff1550e7374e4d9f3f0e666c86a6ff5f4
nyunn2 <jungyun_01@snu.ac.kr>
2025-06-30 22:17:27 +0900

    import 정리

--- app/src/main/java/com/wafflestudio/siksha2/ui/main/restaurant/DailyRestaurantFragment.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/main/restaurant/DailyRestaurantFragment.kt
@@ -2,13 +2,11 @@ package com.wafflestudio.siksha2.ui.main.restaurant
 
 import android.Manifest
 import android.animation.ObjectAnimator
-import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.*
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.collectAsState
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
