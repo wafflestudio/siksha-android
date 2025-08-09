e062b85c89ebad73d0abaa493e39720b50d8d9b8
Shim Woojin <wjshim@snu.ac.kr>
2025-08-09 23:10:38 +0900

    FilterDialogFragment에 newInstance 추가

--- app/src/main/java/com/wafflestudio/siksha2/ui/main/restaurant/FilterDialogFragment.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/main/restaurant/FilterDialogFragment.kt
@@ -31,9 +31,24 @@ import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.DialogFragment
 import java.util.Locale
 
-class FilterDialogFragment(
-    private val mode: FilterMode
-) : DialogFragment() {
+class FilterDialogFragment() : DialogFragment() {
+
+    companion object {
+        private const val DEFAULT_MODE = "FULL"
+
+        fun newInstance(type: FilterMode): FilterDialogFragment {
+            return FilterDialogFragment().apply {
+                arguments = Bundle().apply {
+                    putString(DEFAULT_MODE, type.name)
+                }
+            }
+        }
+    }
+
+    private val mode: FilterMode by lazy {
+        enumValueOf(requireArguments().getString(DEFAULT_MODE)!!)
+    }
+
     private var _binding: DialogFilterBinding? = null
     private val binding get() = _binding!!
 
