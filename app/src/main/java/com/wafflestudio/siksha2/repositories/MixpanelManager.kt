424dd55aa8acf5c1e487b7b59d787345fee8c0fe
kevin990222 <kevin9922@snu.ac.kr>
2025-08-10 12:13:27 +0900

    Mixpanel 적용

---
+++ app/src/main/java/com/wafflestudio/siksha2/repositories/MixpanelManager.kt
@@ -0,0 +1,32 @@
+package com.wafflestudio.siksha2.repositories
+
+import android.content.Context
+import com.mixpanel.android.mpmetrics.MixpanelAPI
+import com.wafflestudio.siksha2.BuildConfig
+import com.wafflestudio.siksha2.R
+import dagger.hilt.android.qualifiers.ApplicationContext
+import org.json.JSONObject
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class MixpanelManager @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    private val token: String =
+        when (BuildConfig.BUILD_TYPE) {
+            "release" -> context.getString(R.string.mixpanel_token_release)
+            else -> context.getString(R.string.mixpanel_token_debug)
+        }
+
+    private val mp: MixpanelAPI =
+        MixpanelAPI.getInstance(context, token, false)
+
+    fun track(eventName: String, props: JSONObject? = null) {
+        if (props != null) {
+            mp.track(eventName, props)
+        } else {
+            mp.track(eventName)
+        }
+    }
+}
