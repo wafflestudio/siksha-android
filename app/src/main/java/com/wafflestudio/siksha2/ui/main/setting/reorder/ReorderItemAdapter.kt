43e3811fde9dd71448ccd6512ebcf347a5b0c9b9
Shim Woojin <wjshim@snu.ac.kr>
2025-08-27 00:44:26 +0900

    3차 QA ReorderRestaurantFragment

--- app/src/main/java/com/wafflestudio/siksha2/ui/main/setting/reorder/ReorderItemAdapter.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/main/setting/reorder/ReorderItemAdapter.kt
@@ -1,6 +1,7 @@
 package com.wafflestudio.siksha2.ui.main.setting.reorder
 
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.wafflestudio.siksha2.R
 import com.wafflestudio.siksha2.databinding.ItemSettingRestaurantReorderBinding
 import com.wafflestudio.siksha2.utils.getInflater
@@ -20,6 +21,20 @@ class ReorderItemAdapter() : DragItemAdapter<Pair<Long, String>, ReorderItemAdap
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         super.onBindViewHolder(holder, position)
         holder.binding.restaurantName.text = mItemList[position].second
+
+        val nameBg = when (position) {
+            0 -> R.drawable.frame_top_left_corner_radius_12
+            itemCount - 1 -> R.drawable.frame_bottom_left_corner_radius_12
+            else -> R.color.gray_50
+        }
+        val handleBg = when (position) {
+            0 -> R.drawable.frame_top_right_corner_radius_12
+            itemCount - 1 -> R.drawable.frame_bottom_right_corner_radius_12
+            else -> R.color.background_secondary
+        }
+
+        holder.binding.restaurantNameBackground.background = ContextCompat.getDrawable(holder.itemView.context, nameBg)
+        holder.binding.reorderHandleBackground.background = ContextCompat.getDrawable(holder.itemView.context, handleBg)
     }
 
     override fun getUniqueItemId(position: Int): Long {
