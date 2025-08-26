adaa567839e94f618681ee149a5885dc91ce69ce
Shim Woojin <wjshim@snu.ac.kr>
2025-08-27 00:18:01 +0900

    grayicon, tooltip2 색 추가

--- app/src/main/java/com/wafflestudio/siksha2/ui/SikshaColors.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/SikshaColors.kt
@@ -117,6 +117,8 @@ class SikshaColors(
         get() = if (isDarkTheme) Gray50 else Gray100
     val ElementTooltip: Color
         get() = if (isDarkTheme) Gray400 else Gray100
+    val ElementTooltip2: Color
+        get() = if (isDarkTheme) Gray400 else White
     val ElementControl: Color
         get() = if (isDarkTheme) Gray500 else Gray200
 
