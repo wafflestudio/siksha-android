1160b6e9c01b66ed8a25440b9e0ffa9c59b54a56
Shim Woojin <wjshim@snu.ac.kr>
2025-08-27 16:01:10 +0900

    메뉴 통계 컴포넌트

---
+++ app/src/main/java/com/wafflestudio/siksha2/compose/ui/menudetail/MenuKeywordStats.kt
@@ -0,0 +1,87 @@
+package com.wafflestudio.siksha2.compose.ui.menudetail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.wafflestudio.siksha2.ui.CancelIcon
+import com.wafflestudio.siksha2.ui.SikshaTheme
+
+@Composable
+fun MenuKeywordStat(
+    keywordString: String,
+    keywordCount: Long,
+    keywordIcon: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.background(color = SikshaTheme.colors.Gray100, shape = RoundedCornerShape(8.dp))
+            .padding(top = 6.dp, bottom = 6.dp, start = 14.dp, end = 20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        keywordIcon()
+        Spacer(Modifier.width(6.dp))
+        Text(
+            text = keywordString,
+            fontSize = 13.sp,
+            fontWeight = FontWeight.Bold,
+            color = SikshaTheme.colors.Gray600,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = keywordCount.toString(),
+            fontSize = 14.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = SikshaTheme.colors.Orange500
+        )
+    }
+}
+
+@Composable
+fun MenuKeywordStats(
+    keywords: List<String>,
+    keywordCounts: List<Long>,
+    keywordIcons: List<@Composable () -> Unit>,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        for (i in keywords.indices) {
+            MenuKeywordStat(
+                keywordString = keywords[i],
+                keywordCount = keywordCounts[i],
+                keywordIcon = keywordIcons[i]
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun MenuKeywordStatsPreview() {
+    MenuKeywordStats(
+        keywords = listOf("맛", "가격", "음식구성"),
+        keywordCounts = listOf(12, 3, 1),
+        keywordIcons = listOf(
+            { CancelIcon() },
+            { CancelIcon() },
+            { CancelIcon() }
+        ),
+        modifier = Modifier.height(150.dp)
+    )
+}
---
+++ app/src/main/java/com/wafflestudio/siksha2/compose/ui/menudetail/MenuRatingsInfo.kt
@@ -0,0 +1,61 @@
+package com.wafflestudio.siksha2.compose.ui.menudetail
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.wafflestudio.siksha2.R
+import com.wafflestudio.siksha2.ui.SikshaTheme
+import java.util.Locale
+
+@Composable
+fun MenuRatingsInfo(
+    rating: Float,
+    reviewCount: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.defaultMinSize(minWidth = 113.dp, minHeight = 120.dp)
+            .border(width = 1.dp, shape = RoundedCornerShape(16.dp), color = SikshaTheme.colors.Gray200),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = String.format(Locale.getDefault(), "%.1f", rating),
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = SikshaTheme.colors.Black
+        )
+        MenuRatingStars(
+            initialRating = rating,
+            width = 72.dp,
+            height = 12.dp
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(
+            text = stringResource(R.string.menu_detail_review_prefix) + " " + reviewCount.toString() + stringResource(R.string.menu_detail_review_suffix),
+            fontSize = 14.sp
+        )
+    }
+}
+
+@Composable
+@Preview
+fun MenuRatingsInfoPreview() {
+    MenuRatingsInfo(
+        rating = 4.0f,
+        reviewCount = 47
+    )
+}
