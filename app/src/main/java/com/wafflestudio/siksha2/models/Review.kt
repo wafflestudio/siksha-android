f849784b00baeebf4b316c638a174bab9359a4f9
kevin990222 <113081855+kevin990222@users.noreply.github.com>
2025-10-12 18:35:16 +0900

    review.kt 수정

--- app/src/main/java/com/wafflestudio/siksha2/models/Review.kt
+++ app/src/main/java/com/wafflestudio/siksha2/models/Review.kt
@@ -6,8 +6,8 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class Review(
     @Json(name = "id") val id: Long,
-    @Json(name = "menu_id") val menuId: Long,
-    @Json(name = "user_id") val userId: Long,
+    @Json(name = "menuId") val menuId: Long,
+    @Json(name = "userId") val userId: Long,
     @Json(name = "score") val score: Double,
     @Json(name = "comment") val comment: String?,
     @Json(name = "keywordReviews") val keywordReviews: List<String>?,
@@ -15,8 +15,8 @@ data class Review(
     @Json(name = "isLiked") val isLiked: Boolean?,
     @Json(name = "nameKr") val nameKr: String?,
     @Json(name = "nameEn") val nameEn: String?,
-    @Json(name = "created_at") val createdAt: String,
-    @Json(name = "updated_at") val updatedAt: String,
+    @Json(name = "createdAt") val createdAt: String,
+    @Json(name = "updatedAt") val updatedAt: String,
     @Json(name = "etc") val etc: Etc?
 )
 
