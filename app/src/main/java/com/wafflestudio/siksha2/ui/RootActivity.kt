965c421c86b1c328df04f1d44285baaf9edb7989
오정윤 <165873171+nyunn2@users.noreply.github.com>
2026-07-13 21:14:45 +0900

    오류 수정 (#169)
    * 상태바 가림 오류 수정
    
    * alias 문제 수정
    
    * 상단바 통합
    
    * 인코딩 오류 수정
    
    * 축제 로직 복구
    
    * 키워드 empty list 오류 해결
    
    * 나의 평가 관리 애니메이션 추가

--- app/src/main/java/com/wafflestudio/siksha2/ui/RootActivity.kt
+++ app/src/main/java/com/wafflestudio/siksha2/ui/RootActivity.kt
@@ -3,6 +3,7 @@ package com.wafflestudio.siksha2.ui
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
@@ -11,6 +12,8 @@ import androidx.lifecycle.lifecycleScope
 import com.wafflestudio.siksha2.R
 import com.wafflestudio.siksha2.repositories.MenuRepository
 import com.wafflestudio.siksha2.repositories.RestaurantRepository
+import com.wafflestudio.siksha2.utils.applyTopMarginForStatusBarSpacing
+import com.wafflestudio.siksha2.utils.applyStatusBarSpacing
 import com.wafflestudio.siksha2.utils.showToast
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -27,6 +30,7 @@ class RootActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_root)
 
         val navHost = findViewById<FragmentContainerView>(R.id.nav_host)
@@ -37,12 +41,27 @@ class RootActivity : AppCompatActivity() {
             )
             v.updatePadding(
                 left = insets.left,
-                top = insets.top,
                 bottom = insets.bottom,
                 right = insets.right
             )
-            WindowInsetsCompat.CONSUMED
+            windowInsets
         }
+        ViewCompat.requestApplyInsets(navHost)
+        supportFragmentManager.registerFragmentLifecycleCallbacks(
+            object : androidx.fragment.app.FragmentManager.FragmentLifecycleCallbacks() {
+                override fun onFragmentViewCreated(
+                    fm: androidx.fragment.app.FragmentManager,
+                    f: androidx.fragment.app.Fragment,
+                    v: android.view.View,
+                    savedInstanceState: Bundle?
+                ) {
+                    v.findViewById<android.view.View?>(R.id.top_bar)?.applyStatusBarSpacing(extraTopDp = 0)
+                    v.findViewById<android.view.View?>(R.id.tool_bar)?.applyStatusBarSpacing(extraTopDp = 0)
+                    v.findViewById<android.view.View?>(R.id.main_logo)?.applyTopMarginForStatusBarSpacing(extraTopDp = 0)
+                }
+            },
+            true
+        )
 
         lifecycleScope.launch {
             menuRepository.sweepOldMenus()
